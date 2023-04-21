8c83bafd2bac3c13750e75a468df518e4f8fd2b8
Rakesh Sharma <rakeshbagda9462@gmail.com>
2023-04-21 17:59:12 +0530

    Html layout

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").toggle('slow');
+    });
+    $("#flip-two").click(function(){
+      $("#panel-two").toggle('slow');
+    });
+    $("#flip-three").click(function(){
+      $("#panel-three").toggle('slow');
+    });
+    $("#flip-four").click(function(){
+      $("#panel-four").toggle('slow');
+    });
+    $("#flip-five").click(function(){
+      $("#panel-five").toggle('slow');
+    });
+    $("#mobile-menu").click(function(){
+      $("#menu-mobile, #overlay-mobile").show('slide');
+      $("body").css('overflow', 'hidden');
+        $("body").addClass('test');
+    });
+    $("#overlay-mobile, #close").click(function(){
+      $("#overlay-mobile, #menu-mobile").hide('slide');
+      $("body").css('overflow', 'auto');
+        $("body").removeClass();
+    });
+  });
